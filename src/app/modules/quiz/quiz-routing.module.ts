3eb5cfb0223b8e0b377e910112c11315464d09e5
Duvan Jamid Vargas <mi13dj22@gmail.com>
2022-12-19 13:01:33 -0500

    se agrega navegacion

--- src/app/modules/quiz/quiz-routing.module.ts
+++ src/app/modules/quiz/quiz-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ListadoQuicesComponent} from "./listado-quices/listado-quices.component";
+import {CrearQuizComponent} from "./crear-quiz/crear-quiz.component";
 
 
-const routes: Routes = [{ path: '', component: ListadoQuicesComponent }];
+const routes: Routes = [
+  { path: '', component: ListadoQuicesComponent },
+  { path: 'crear', component: CrearQuizComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
